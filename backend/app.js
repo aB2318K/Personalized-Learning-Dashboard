574d9d97c4b7ca855f08de8823f8c8b177e87f69
Ansh Baral <busybaral@outlook.com>
2025-03-03 11:15:00 +0000

    imported Goal model in app.js

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import { HfInference } from '@huggingface/inference';
 import { authenticateToken } from './middleware/authMiddleware.js';
+import Goal from './models/Goal.js';
 
 // Configurations
 import connectDB from './config/db.js';
